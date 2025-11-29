13a70a81e93872a00131d03a44325b262f023265
Shaik <shaik@example.com>
2025-11-29 22:31:57 +0530

    Use environment PORT (process.env.PORT) for hosting compatibility

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
